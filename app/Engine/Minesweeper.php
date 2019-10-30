b080b99767017aa806945cd6b63ddc71af936e8d
Gadix <gabriel@gadix.com.ar>
2019-10-30 10:56:18 -0300

    Migrate methods Controller -> Engine

--- app/Engine/Minesweeper.php
+++ app/Engine/Minesweeper.php
@@ -3,10 +3,249 @@
 namespace App\Engine;
 
 
-class Minesweeper {
+class Minesweeper
+{
+
+
+    public function makeGrid($columns, $rows,$mines)
+    {
+
+        $cells = $columns * $rows;
+
+        $grid = array();
+
+        for ($i = 0; $i < $rows; $i++) {
+
+            $line = array();
+
+            for ($v = 0; $v < $columns; $v++) {
+                if ($mines > 0) {
+                    if ($cells > $mines) {
+                        $mine = rand(0, 1);
+                    } else {
+                        $mine = 1;
+                    }
+
+                    if ($mine == 1) {
+                        $mines--;
+                    }
+                } else {
+                    $mine = 0;
+                }
+                array_push($line, $mine);
+                $cells--;
+            }
+
+            array_push($grid, $line);
+        }
+
+        return $grid;
+    }
+
+    public function makeUserGrid($grid)
+    {
+
+        $emptyRow = array();
+        $emptyGrid = array();
+
+        for ($i = 0; $i < sizeof($grid[0]); $i++) {
+
+            array_push($emptyRow, "X");
+        }
+
+        for ($i = 0; $i < sizeof($grid); $i++) {
+            array_push($emptyGrid, $emptyRow);
+        };
+
+        return $emptyGrid;
+    }
 
 
 
 
+    public function calculate($grid, $cell)
+    {
 
+        $count = 0;
+
+        $top = $bottom = FALSE;
+
+        $position = explode('-', $cell);
+
+        $row = $position[0];
+
+        $column = $position[1];
+
+        // Dimensions
+        $rows = sizeof($grid);
+
+        $columns = sizeof($grid[0]);
+
+        // Search in TOP
+
+        if ($row - 1 >= 0) {
+            $top = TRUE;
+            if ($grid[$row - 1][$column] == 1) {
+                $count++;
+            }
+        }
+
+        // Search in Bottom
+
+        if ($row + 1 < $rows) {
+            $bottom = TRUE;
+            if ($grid[$row + 1][$column] == 1) {
+                $count++;
+            }
+        }
+
+        // Search in Left
+
+        if ($column - 1 >= 0) {
+
+            if ($grid[$row][$column - 1] == 1) {
+                $count++;
+            }
+            if ($top) {
+                if ($grid[$row - 1][$column - 1] == 1) {
+                    $count++;
+                }
+            }
+            if ($bottom) {
+                if ($grid[$row + 1][$column - 1] == 1) {
+                    $count++;
+                }
+            }
+        }
+
+        // Search in Right
+
+        if ($column + 1 < $columns) {
+            if ($grid[$row][$column + 1] == 1) {
+                $count++;
+            }
+            if ($top) {
+                if ($grid[$row - 1][$column + 1] == 1) {
+                    $count++;
+                }
+            }
+            if ($bottom) {
+                if ($grid[$row + 1][$column + 1] == 1) {
+                    $count++;
+                }
+            }
+        }
+
+        return $count;
+    }
+
+    public function calculateAdjacentMines($grid, $userGrid, $cell)
+    {
+
+        $position = explode('-', $cell);
+
+        $row = $position[0];
+
+        $column = $position[1];
+
+        // Dimensions
+        $rows = sizeof($grid);
+
+        $columns = sizeof($grid[0]);
+
+        $userGrid[$row][$column] = 0;
+
+        $empty = TRUE;
+
+        /*Line to TOP */
+        $i = 1;
+        while ($empty) {
+
+            if ($row - $i  >= 0) {
+                $cellString = ($row - $i) . "-" . $column;
+                $status = $this->calculate($grid, $cellString);
+                if ($status == 0) {
+                    $userGrid[$row - $i][$column] = 0;
+                } else {
+                    $empty = FALSE;
+                }
+            } else {
+                break;
+            }
+            $i++;
+        }
+
+        // Line to BOTTOM
+        $i = 1;
+        $empty = TRUE;
+        while ($empty) {
+
+            if ($row + $i  < $rows) {
+                $cellString = ($row + $i) . "-" . $column;
+                $status = $this->calculate($grid, $cellString);
+                if ($status == 0) {
+                    $userGrid[$row + $i][$column] = 0;
+                } else {
+                    $empty = FALSE;
+                }
+            } else {
+                break;
+            }
+            $i++;
+        }
+
+
+        // Line to LEFT
+        $i = 1;
+        $empty = TRUE;
+        while ($empty) {
+
+            if ($column -  $i  >= 0) {
+                $cellString = $row . "-" . ($column - $i);
+                $status = $this->calculate($grid, $cellString);
+                if ($status == 0) {
+                    $userGrid[$row][$column - $i] = 0;
+                } else {
+                    $empty = FALSE;
+                }
+            } else {
+                break;
+            }
+            $i++;
+        }
+
+
+        // Line to RIGHT
+        $i = 1;
+        $empty = TRUE;
+        while ($empty) {
+
+            if ($column +  $i   < $columns) {
+                $cellString = $row . "-" . ($column + $i);
+                $status = $this->calculate($grid, $cellString);
+                if ($status == 0) {
+                    $userGrid[$row][$column + $i] = 0;
+                } else {
+                    $empty = FALSE;
+                }
+            } else {
+                break;
+            }
+            $i++;
+        }
+
+        return $userGrid;
+    }
+
+
+
+    public function updateUserGrid($userGrid, $cell, $value)
+    {
+
+        $position = explode('-', $cell);
+
+        $userGrid[$position[0]][$position[1]] = $value;
+
+        return $userGrid;
+    }
 }
--- app/Http/Controllers/MinesweeperController.php
+++ app/Http/Controllers/MinesweeperController.php
@@ -14,16 +14,18 @@ class MinesweeperController extends Controller
 
         $columns = $rows = 5;
 
+        $mines = 1;
+
         $data = [
             "columns" => $columns,
             "rows" => $rows,
         ];
 
-        $grid =  $this->makeGrid($columns, $rows);
+        $grid =  $minesweeper->makeGrid($columns, $rows,$mines);
 
         $request->session()->put("grid", $grid);
 
-        $request->session()->put("userGrid", $this->makeUserGrid($grid));
+        $request->session()->put("userGrid", $minesweeper->makeUserGrid($grid));
 
         $request->session()->put("gameover", false);
 
@@ -31,64 +33,18 @@ class MinesweeperController extends Controller
     }
 
 
-
-    private function makeGrid($columns, $rows)
+    public function getUserGrid(Request $request)
     {
 
-        $mines = 1;
-
-        $cells = $columns * $rows;
-
-        $grid = array();
-
-        for ($i = 0; $i < $rows; $i++) {
-
-            $line = array();
-
-            for ($v = 0; $v < $columns; $v++) {
-                if ($mines > 0) {
-                    if ($cells > $mines) {
-                        $mine = rand(0, 1);
-                    } else {
-                        $mine = 1;
-                    }
-
-                    if ($mine == 1) {
-                        $mines--;
-                    }
-                } else {
-                    $mine = 0;
-                }
-                array_push($line, $mine);
-                $cells--;
-            }
-
-            array_push($grid, $line);
-        }
-
-        return $grid;
+        return response()
+            ->json($request->session()->get("userGrid"));
     }
 
-    public function makeUserGrid($grid)
-    {
-
-        $emptyRow = array();
-        $emptyGrid = array();
-
-        for ($i = 0; $i < sizeof($grid[0]); $i++) {
-
-            array_push($emptyRow, "X");
-        }
-
-        for ($i = 0; $i < sizeof($grid); $i++) {
-            array_push($emptyGrid, $emptyRow);
-        };
-
-        return $emptyGrid;
-    }
 
     public function check(Request $request)
     {
+
+        $minesweeper = new Minesweeper();
 
         if ($request->session()->get("gameover")) {
             return response()
@@ -110,12 +66,12 @@ class MinesweeperController extends Controller
 
             $request->session()->push("gameover", true);
         } else {
-            $status = $this->calculate($grid, $cell);
+            $status = $minesweeper->calculate($grid, $cell);
 
             if ($status == 0) {
-               $request->session()->put("userGrid",$this->calculateAdjacentMines($grid, $userGrid, $cell));
+                $request->session()->put("userGrid", $minesweeper->calculateAdjacentMines($grid, $userGrid, $cell));
             } else {
-                $request->session()->put("userGrid", $this->updateUserGrid($userGrid, $cell, $status));
+                $request->session()->put("userGrid", $minesweeper->updateUserGrid($userGrid, $cell, $status));
             }
         }
 
@@ -123,205 +79,4 @@ class MinesweeperController extends Controller
             ->json($status);
     }
 
-    public function getUserGrid(Request $request)
-    {
-
-        return response()
-            ->json($request->session()->get("userGrid"));
-    }
-
-    private function calculate($grid, $cell)
-    {
-
-        $count = 0;
-
-        $top = $bottom = FALSE;
-
-        $position = explode('-', $cell);
-
-        $row = $position[0];
-
-        $column = $position[1];
-
-        // Dimensions
-        $rows = sizeof($grid);
-
-        $columns = sizeof($grid[0]);
-
-        // Search in TOP
-
-        if ($row - 1 >= 0) {
-            $top = TRUE;
-            if ($grid[$row - 1][$column] == 1) {
-                $count++;
-            }
-        }
-
-        // Search in Bottom
-
-        if ($row + 1 < $rows) {
-            $bottom = TRUE;
-            if ($grid[$row + 1][$column] == 1) {
-                $count++;
-            }
-        }
-
-        // Search in Left
-
-        if ($column - 1 >= 0) {
-
-            if ($grid[$row][$column - 1] == 1) {
-                $count++;
-            }
-            if ($top) {
-                if ($grid[$row - 1][$column - 1] == 1) {
-                    $count++;
-                }
-            }
-            if ($bottom) {
-                if ($grid[$row + 1][$column - 1] == 1) {
-                    $count++;
-                }
-            }
-        }
-
-        // Search in Right
-
-        if ($column + 1 < $columns) {
-            if ($grid[$row][$column + 1] == 1) {
-                $count++;
-            }
-            if ($top) {
-                if ($grid[$row - 1][$column + 1] == 1) {
-                    $count++;
-                }
-            }
-            if ($bottom) {
-                if ($grid[$row + 1][$column + 1] == 1) {
-                    $count++;
-                }
-            }
-        }
-
-        return $count;
-    }
-
-    private function calculateAdjacentMines($grid, $userGrid, $cell)
-    {
-
-        $position = explode('-', $cell);
-
-        $row = $position[0];
-
-        $column = $position[1];
-
-        // Dimensions
-        $rows = sizeof($grid);
-
-        $columns = sizeof($grid[0]);
-
-        $userGrid[$row][$column] = 0;
-
-        $empty = TRUE;
-
-        /*Line to TOP */
-        $i = 1;
-        while ($empty) {
-
-            if ($row - $i  >= 0) {
-                $cellString = ($row - $i) . "-" . $column;
-                $status = $this->calculate($grid, $cellString);
-                if ($status == 0) {
-                    $userGrid[$row - $i][$column] = 0;
-
-                } else {
-                    $empty = FALSE;
-                }
-            } else {
-                break;
-            }
-            $i++;
-        }
-
-        // Line to BOTTOM
-        $i = 1;
-        $empty = TRUE;
-        while ($empty) {
-
-            if ($row + $i  < $rows) {
-                $cellString = ($row + $i) . "-" . $column;
-                $status = $this->calculate($grid, $cellString);
-                if ($status == 0) {
-                    $userGrid[$row + $i][$column] = 0;
-
-                } else {
-                    $empty = FALSE;
-                }
-            } else {
-                break;
-            }
-            $i++;
-        }
-
-
-        // Line to LEFT
-        $i = 1;
-        $empty = TRUE;
-        while ($empty) {
-
-            if ($column -  $i  >= 0) {
-                $cellString = $row . "-" . ($column - $i);
-                $status = $this->calculate($grid, $cellString);
-                if ($status == 0) {
-                    $userGrid[$row][$column - $i] = 0;
-
-                } else {
-                    $empty = FALSE;
-                }
-            } else {
-                break;
-            }
-            $i++;
-        }
-
-
-        // Line to RIGHT
-        $i = 1;
-        $empty = TRUE;
-        while ($empty) {
-
-            if ($column +  $i   < $columns) {
-                $cellString = $row . "-" . ($column + $i);
-                $status = $this->calculate($grid, $cellString);
-                if ($status == 0) {
-                    $userGrid[$row ][$column + $i] = 0;
-
-                } else {
-                    $empty = FALSE;
-                }
-            } else {
-                break;
-            }
-            $i++;
-        }
-
-        return $userGrid;
-
-    }
-
-    private function calculateLeftMines($grid,$column,$row){
-
-
-
-    }
-
-    private function updateUserGrid($userGrid, $cell, $value)
-    {
-
-        $position = explode('-', $cell);
-
-        $userGrid[$position[0]][$position[1]] = $value;
-
-        return $userGrid;
-    }
 }
