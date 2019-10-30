82373b60ba11554df27501a5997bcfc459ddb5ac
Gadix <gabriel@gadix.com.ar>
2019-10-30 11:16:18 -0300

    Generate API Folders/ Namespaces / Controller

---
+++ app/Http/Controllers/API/Minesweeper/V1/MinesweeperAPIController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\API\Minesweeper\V1;
+
+use Illuminate\Http\Request;
+use App\Engine\Minesweeper ;
+use App\Http\Controllers\Controller;
+
+class MinesweeperAPIController extends Controller
+{
+    public function new(){
+        return "NEW METHOD";
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::prefix('minesweeper/V1')->group(function () {
+    Route::get('new', 'API\Minesweeper\V1\MinesweeperAPIController@new');
+});
