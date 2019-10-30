d46dc12c5a7895512806dfef77a8ad8e2974f701
Gadix <gabriel@gadix.com.ar>
2019-10-30 10:47:48 -0300

    Add Engine Folder and Minesweeper Class

--- app/Http/Controllers/MinesweeperController.php
+++ app/Http/Controllers/MinesweeperController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Engine\Minesweeper ;
 
 class MinesweeperController extends Controller
 {
     public function index(Request $request)
     {
+
+        $minesweeper = new Minesweeper();
 
         $columns = $rows = 5;
 
